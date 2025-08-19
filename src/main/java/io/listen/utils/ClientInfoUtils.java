1e6ac4b6f36b654b88df041511f724998225d285
Irreplaceable <1349694984@qq.com>
2025-08-19 22:16:11 +0800

    update ci yml

--- src/main/java/io/listen/utils/ClientInfoUtils.java
+++ src/main/java/io/listen/utils/ClientInfoUtils.java
@@ -96,7 +96,7 @@ public class ClientInfoUtils {
     public Map<String, String> getClientInfo(String userAgent) {
         Map<String, String> map = new HashMap<>();
         DeviceDetectorResult result = parser.parse(userAgent);
-        //test
+        //test 2
         Map<String, String> resultMap = result.toMap();
         //设备类型
         map.put("device", resultMap.get("device.deviceType"));
