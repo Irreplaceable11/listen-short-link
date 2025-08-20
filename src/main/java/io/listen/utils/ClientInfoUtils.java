17e0091db809ad289560f2ba2c266dcf2248361f
Irreplaceable <1349694984@qq.com>
2025-08-20 10:35:08 +0800

    test native with cache2

--- src/main/java/io/listen/utils/ClientInfoUtils.java
+++ src/main/java/io/listen/utils/ClientInfoUtils.java
@@ -96,7 +96,7 @@ public class ClientInfoUtils {
     public Map<String, String> getClientInfo(String userAgent) {
         Map<String, String> map = new HashMap<>();
         DeviceDetectorResult result = parser.parse(userAgent);
-        //test native
+        //test native with cache
         Map<String, String> resultMap = result.toMap();
         //设备类型
         map.put("device", resultMap.get("device.deviceType"));
